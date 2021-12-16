02e77ded87ea842fc16baf3429d7355994fcf876
Morris <yiiyatechuganda@gmail.com>
2021-12-16 09:26:16 +0300

    initial live

---
+++ frontend/client-server/src/views/layouts/navigation-header/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import GoogleAuth from '../../components/google-auth'
+
+const NavigationHeader = () => <div></div>
+  // <div className="top-bar shadow">
+  //   <div className="top-bar-left">
+  //     <ul className="menu">
+  //       <li className="menu-text">WGTV Live</li>
+  //     </ul>
+  //   </div>
+  //   <div className="top-bar-right">
+  //     <ul className="menu">
+  //       <li><GoogleAuth /></li>
+  //     </ul>
+  //   </div>
+  // </div>;
+
+export default NavigationHeader;
