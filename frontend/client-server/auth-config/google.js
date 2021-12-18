2fef0bcb3fa362b864f8235fffeca33f238d1b52
Morris <yiiyatechuganda@gmail.com>
2021-12-19 00:42:58 +0300

    More

---
+++ frontend/client-server/auth-config/google.js
@@ -0,0 +1,7 @@
+/* Google Credentials */
+
+const googleCredentials = {
+  clientId: '811107924326-vgioa4fdaq6icjqppcfon5t0rj2blfor.apps.googleusercontent.com',
+};
+
+export default googleCredentials;
