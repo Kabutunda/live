66660216ce78ec958862f851f98320f31a6a26f7
Morris <yiiyatechuganda@gmail.com>
2022-01-09 18:49:06 +0300

    stuck now

--- server.js
+++ server.js
@@ -22,10 +22,10 @@ const nms = new NodeMediaServer(config);
 
 nms.run();
 
-// heroku setup
-if (process.env.NODE_ENV==='production') {
-  app.use(express.static('client/build'));
-  app.get("*", (req, res) =>{
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  })
-}
+//heroku setup
+// if (process.env.NODE_ENV==='production') {
+//   app.use(express.static('client/build'));
+//   app.get("*", (req, res) =>{
+//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+//   })
+// }
